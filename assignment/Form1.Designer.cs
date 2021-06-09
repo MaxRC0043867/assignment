189d9a6e5cfd236b5704140db762e60dd6b58e56
Max Welsby West <RC0043867@student.riversidecollege.ac.uk>
2021-06-09 10:56:54 +0100

    Add project files.

---
+++ assignment/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.cuboid = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sphere = new System.Windows.Forms.TabPage();
+            this.cone = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.cuboid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.cuboid);
+            this.tabControl1.Controls.Add(this.sphere);
+            this.tabControl1.Controls.Add(this.cone);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // cuboid
+            // 
+            this.cuboid.Controls.Add(this.pictureBox1);
+            this.cuboid.Controls.Add(this.label2);
+            this.cuboid.Controls.Add(this.label1);
+            this.cuboid.Controls.Add(this.button1);
+            this.cuboid.Controls.Add(this.textBox3);
+            this.cuboid.Controls.Add(this.textBox2);
+            this.cuboid.Controls.Add(this.textBox1);
+            this.cuboid.Location = new System.Drawing.Point(4, 22);
+            this.cuboid.Name = "cuboid";
+            this.cuboid.Size = new System.Drawing.Size(792, 424);
+            this.cuboid.TabIndex = 0;
+            this.cuboid.Text = "cuboid";
+            this.cuboid.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::assignment.Properties.Resources.MicrosoftTeams_image;
+            this.pictureBox1.Location = new System.Drawing.Point(75, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 265);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(609, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(591, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(591, 325);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(445, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // sphere
+            // 
+            this.sphere.Location = new System.Drawing.Point(4, 22);
+            this.sphere.Name = "sphere";
+            this.sphere.Size = new System.Drawing.Size(792, 424);
+            this.sphere.TabIndex = 1;
+            this.sphere.Text = "sphere";
+            this.sphere.UseVisualStyleBackColor = true;
+            // 
+            // cone
+            // 
+            this.cone.Location = new System.Drawing.Point(4, 22);
+            this.cone.Name = "cone";
+            this.cone.Size = new System.Drawing.Size(792, 424);
+            this.cone.TabIndex = 2;
+            this.cone.Text = "cone";
+            this.cone.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.cuboid.ResumeLayout(false);
+            this.cuboid.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage cuboid;
+        private System.Windows.Forms.TabPage sphere;
+        private System.Windows.Forms.TabPage cone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ assignment/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //read length, width and height from textboxes
+                double length = double.Parse(textBox1.Text);
+                double width = double.Parse(textBox2.Text);
+                double height = double.Parse(textBox3.Text);
+                // calculation of area and volume
+                double area = 2.0 * ((length * width) + (length * height) + (width * height));
+                double volume = length * width * height;
+                // put values in labels
+                label1.Text = "surface area = " + area;
+                label2.Text = "volume = " + volume;
+
+
+            }
+            catch
+            {
+                // show error message on screen if fail
+                MessageBox.Show("Error! Error!! Error!!!!!!!");
+
+            }
+        }
+
+        }
+    }
+
